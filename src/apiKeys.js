33ec13e78120c99bb15efa71f3b48767b9334e33
eaglespire <66023850+eaglespire@users.noreply.github.com>
2020-09-21 04:40:01 +0100

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
+// Visit https://api.openweathermap.org & then signup to get your own API keys for free
 module.exports = {
-  key: "05a946da8e6f4aa8b1ef5e720d3c696d",
+  key: {ADD YOUR API KEY HERE},
   base: "https://api.openweathermap.org/data/2.5/",
 };
